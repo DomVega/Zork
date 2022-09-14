33a1d8a1e46e98b531bb82d885e2e22407a7562d
DomVega <70494132+DomVega@users.noreply.github.com>
2022-09-14 15:18:58 -0400

    Zork 2.2 - 2nd commit
    saved changes made to Room class

--- Zork/Room.cs
+++ Zork/Room.cs
@@ -6,5 +6,19 @@ namespace Zork
 {
     internal class Room
     {
+        public string Name { get; set; }
+        
+        public string Description { get; set; }
+
+        public Room(string name,string description = null)
+        {
+            Name = name;
+            Description = description;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
     }
 }
