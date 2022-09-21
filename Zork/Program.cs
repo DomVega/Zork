5020ffa8daf2e716007d775b1c911f686c6f4206
DomVega <70494132+DomVega@users.noreply.github.com>
2022-09-21 14:35:30 -0400

    Zork 3.1 - Second commit
    Changed room array. added room descriptions

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -100,14 +100,24 @@ namespace Zork
 
         private static void InitializeRoomDescriptions()
         {
-            //_rooms[0, 0].Description = "You are onn a rock-strewn trail.";
+            _rooms[0, 0].Description = "You are onn a rock-strewn trail.";
+            _rooms[0, 1].Description = "You are facing the south side of a whir]te house. There is no door here, and all the windows are barred.";      // Rocky Trail
+            _rooms[0, 2].Description = "You are at the top of the Great Canyon on its south wall";                                                      // South of House
+
+            _rooms[1, 0].Description = "This is a forest, with threes in all directions around you.";                                                   // Forest
+            _rooms[1, 1].Description = "This is an open field west of a white house, with a boarded front door.";                                       // West of House
+            _rooms[1, 2].Description = "You are behind the white house. in one corner of the house there is a small window which is slightly ajar.";    // Behind House
+
+            _rooms[2, 0].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";           // Dense Woods
+            _rooms[2, 1].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred.";        // North of House
+            _rooms[2, 2].Description = "You are in a clearing, with a froest surrounding yo on the west and south.";                                    // Clearing
         }
 
-        private static readonly string[,] _rooms = 
+        private static readonly Room[,] _rooms = 
         {
-            { "Rocky Trail", "South of House", "Canyon View" },
-            { "Forest", "West of House", "Behind House"},
-            { "Dense Woods", "North of House","Clearing" }
+            { new Room ("Rocky Trail"), new Room("South of House"), new Room("Canyon View") },
+            { new Room("Forest"), new Room("West of House"), new Room("Behind House") },
+            { new Room("Dense Woods"), new Room("North of House"), new Room("Clearing") }
         };
         private static (int Row, int Column) Location = (1,1 ); 
     }
